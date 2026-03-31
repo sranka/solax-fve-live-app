e30ef824b7e3dd25ed856c23f5ab04387af53614
Pavel Zavora <pavel.zavora@gmail.com>
2026-03-31 14:53:08 +0200

    feat: appear better on 16:9 resolutions

--- scripts/stamp-build.sh
+++ scripts/stamp-build.sh
@@ -7,7 +7,7 @@ SHA1=$(cat web/index.html web/sw.js | shasum | cut -c1-8)
 BUILDTIME=$(date '+%Y-%m-%d %H:%M')
 
 cat > web/build-info.js <<EOF
-document.getElementById('buildFingerprint').textContent = 'build: v$VERSION $SHA1 ($BUILDTIME)';
+document.getElementById('buildFingerprint').textContent = 'build: v$VERSION $SHA1 ($BUILDTIME) ' + screen.width + 'x' + screen.height;
 EOF
 
 echo "Generated build-info.js with fingerprint: v$VERSION $SHA1 ($BUILDTIME)"
